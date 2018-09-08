d29cc9bbfadff90c9c9b12d916a09f0e6ff8de65
Christian Roese <croese@gmail.com>
2018-09-08 16:15:58 -0500

    Adds test proj, Rope class, Length, ToString, and tests

---
+++ NRope.Test/RopeTests.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Linq;
+using Xunit;
+
+namespace NRope.Test
+{
+  public class RopeTests
+  {
+    [Theory]
+    [InlineData("")]
+    [InlineData("hello")]
+    public void RopeHasALength(string str)
+    {
+      var r = new Rope(str);
+      Assert.Equal(str.Length, r.Length);
+    }
+
+    [Theory]
+    [InlineData("hello", " world!", "hello world!")]
+    [InlineData("t", "otally", "e", "fficient", "totallyefficient")]
+    public void RopesCanBeJoined(params string[] ss)
+    {
+      var expected = ss.Last();
+      var first = ss.First();
+      var args = ss.Skip(1).Take(ss.Length - 2);
+
+      var r = args.Aggregate(new Rope(first),
+                             (accum, cur) => accum.Join(new Rope(cur)));
+      Assert.Equal(expected.Length, r.Length);
+      Assert.Equal(expected, r.ToString());
+    }
+  }
+}
---
+++ NRope/Rope.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace NRope
+{
+  public class Rope
+  {
+    readonly string _data;
+    readonly Rope _left;
+    readonly Rope _right;
+
+    public Rope(string s)
+    {
+      _data = s;
+      Length = s.Length;
+    }
+
+    Rope(Rope left, Rope right)
+    {
+      _left = left;
+      _right = right;
+      Length = left.Length + right.Length;
+    }
+
+    public int Length { get; }
+
+    public Rope Join(Rope other)
+    {
+      return new Rope(this, other);
+    }
+
+    void ToString(StringBuilder builder)
+    {
+      if (_data != null)
+      {
+        builder.Append(_data);
+      }
+
+      if (_left != null)
+      {
+        _left.ToString(builder);
+      }
+
+      if (_right != null)
+      {
+        _right.ToString(builder);
+      }
+    }
+
+    public override string ToString()
+    {
+      var sb = new StringBuilder(Length);
+      ToString(sb);
+      return sb.ToString();
+    }
+  }
+}
